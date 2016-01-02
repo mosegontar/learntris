42bd250cfeddf7f94072580dc13932ec617b8113
Alex Gontar <mosegontar@gmail.com>
2016-01-02 14:20:36 -0500

    Pass test 3

--- learntris.py
+++ learntris.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
+import sys
+
+class Grid(object):
+
+    def __init__(self):
+        pass
+
+
+def quit():
+    pass
 
 def draw_board():
     for row in range(0, 22):
         print '. '*10 
+    #print "DRAWING"
+
+def given():
+    
+    for ticker in range(0, 22):
+        print raw_input()
 
 def main():
     
-    command = raw_input()
+    grid = Grid()
+
+    commands = {'g':quit, 'p':draw_board, 'g':given, 'c':draw_board}
     
-    if command != 'q':
-        draw_board()
-
-
+    command = raw_input()
+    commands[command]()
 
 
 
