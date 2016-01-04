efb371503bb275708abcee06d92cfaece4681956
Alex Gontar <mosegontar@gmail.com>
2016-01-04 08:10:25 -0500

    Test 6 passed

--- learntris.py
+++ learntris.py
@@ -5,33 +5,44 @@ import sys
 class Grid(object):
 
     def __init__(self):
-        pass
+        self.board = ['. '*10 for row in range(0,22)]
+        self.score = 0
+        self.lines_clear = 0
 
+    def draw_board(self):
 
-def quit():
-    pass
+        for cell in self.board:
+            print cell
 
-def draw_board():
-    for row in range(0, 22):
-        print '. '*10 
-    #print "DRAWING"
+    def given(self):
+        self.board = []
+        for row in range(0,22):
+            self.board.append(raw_input())
 
-def given():
-    
-    for ticker in range(0, 22):
-        print raw_input()
+    def clear(self):
+        self.board = ['. '*10 for row in range(0,22)]
+
+    def show_score(self):
+        print self.score
+
+    def show_clear_lines(self):
+        print self.lines_clear
 
 def main():
     
     grid = Grid()
 
-    commands = {'g':quit, 'p':draw_board, 'g':given, 'c':draw_board}
-    
-    command = raw_input()
-    commands[command]()
+    commands = {'p': grid.draw_board, 
+                'g': grid.given, 
+                'c': grid.clear,
+                '?s': grid.show_score,
+                '?n': grid.show_clear_lines}
 
-
-
+    while True:
+        command = raw_input()
+        if command == 'q':
+            break
+        commands[command]()
 
 
 if __name__ == '__main__':
